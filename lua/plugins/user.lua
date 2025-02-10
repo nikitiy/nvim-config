85e0bc78154adcc54cb825085222f0a036848a8e
nickoder <nikita_02.20@mail.ru>
2025-02-11 00:38:17 +0300

    add projects plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -11,9 +11,7 @@ return {
     "goolord/alpha-nvim",
     event = "VimEnter",
     enabled = true,
-    config = function()
-      require("plugins.dashboard").config()
-    end,
+    config = function() require("plugins.dashboard").config() end,
   },
 
   { "max397574/better-escape.nvim", enabled = false },
@@ -30,23 +28,23 @@ return {
   {
     "windwp/nvim-autopairs",
     config = function(plugin, opts)
-      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts) 
+      require "astronvim.plugins.configs.nvim-autopairs"(plugin, opts)
       local npairs = require "nvim-autopairs"
       local Rule = require "nvim-autopairs.rule"
       local cond = require "nvim-autopairs.conds"
-      npairs.add_rules(
-        {
-          Rule("$", "$", { "tex", "latex" })
-            :with_pair(cond.not_after_regex "%%")
-            :with_pair(
-              cond.not_before_regex("xxx", 3)
-            )
-            :with_move(cond.none())
-            :with_del(cond.not_after_regex "xx")
-            :with_cr(cond.none()),
-        },
-        Rule("a", "a", "-vim")
-      )
+      npairs.add_rules({
+        Rule("$", "$", { "tex", "latex" })
+          :with_pair(cond.not_after_regex "%%")
+          :with_pair(cond.not_before_regex("xxx", 3))
+          :with_move(cond.none())
+          :with_del(cond.not_after_regex "xx")
+          :with_cr(cond.none()),
+      }, Rule("a", "a", "-vim"))
     end,
+  },
+
+  {
+    "ahmedkhalf/project.nvim",
+    config = function() require("project_nvim").setup {} end,
   },
 }
